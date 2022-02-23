626fec98838423541bc11740be3b11ccd16524c4
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-02-23 23:37:31 +0100

    Updates

--- src/components/card/card.component.jsx
+++
@@ -1,9 +0,0 @@
-import './card.styles.css';
-
-export const Card = props => (
-    <div className='card-container'>
-        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt={`monster${props.monster.id}`} />
-        <h2>{ props.monster.name }</h2>
-        <p>{ props.monster.email }</p>
-    </div>
-);
